2e2310c989d3406b29eb09cb8864f06d41dc937d
SJ Mac Pro <sjcaptain@SJui-MacBookPro.local>
2024-05-08 23:47:06 +0900

    Home page update (RC image added)

--- hgu_tmaew_front/src/page/Home.jsx
+++ hgu_tmaew_front/src/page/Home.jsx
@@ -1,19 +1,64 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { Parallax } from 'react-parallax';
 import './css/Home.css'; // 홈 페이지 스타일을 위한 CSS 파일
+import logo from '../assets/logo_white.svg'; // 이미지의 상대 경로
+import kuyper from '../assets/dorm/kuyper.jpg';
+import torrey from '../assets/dorm/torrey.jpg';
+import carmichael from '../assets/dorm/carmichael.jpg';
+import bethel from '../assets/dorm/bethel.jpg';
+import yeolsong from '../assets/dorm/yeolsong.jpg';
+import grace from '../assets/dorm/grace.jpg';
 
 const Home = () => {
+
+  function ParallaxComponent({ url, title }) {
+    return (
+      <Parallax bgImage={url} strength={1000} bgImageStyle={{ backgroundSize: 'contain', backgroundRepeat: 'no-repeat', backgroundPosition: 'center' }}>
+        <div className="parallax-container">
+          <div className="parallax-title">{title}</div>
+        </div>
+      </Parallax>
+    );
+  }
+
   return (
     <div className="home">
       <header className="home-header">
-        <img src="/img/logo_white.svg" className="home-logo" alt="logo" />
-        {/* <Link to="/login" className="loginButton">로그인</Link> */}
+        <img src={logo} className="home-logo" alt="logo" />
       </header>
       <main className="home-content">
-        {/* 홈페이지의 여러 글이나 기능들을 여기서부터 추가하면 됨!!! */}
-        <p>Handong Team Meeting Archiving<br />And Exchange Web Platform</p>
+        <p className="home-content-title">Handong Team Meeting Archiving<br />And Exchange Web Platform</p>
+        <p className="home-content-subTitle">This platform will support the student support team and team executives in<br /> fostering a stronger team culture and developing leadership skills university-wide.</p>
         <Link to="/login" className="loginButton">Start</Link>
       </main>
+
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+
+      <div className="home-dormInfo">
+        <ParallaxComponent url={kuyper} title="Kuyper" />
+        <ParallaxComponent url={torrey} title="Torrey" />
+        <ParallaxComponent url={carmichael} title="Carmichael" />
+        <ParallaxComponent url={bethel} title="Bethel" />
+        <ParallaxComponent url={yeolsong} title="Yeolsong" />
+        <ParallaxComponent url={grace} title="Grace" />
+      </div>
     </div>
   );
 };
