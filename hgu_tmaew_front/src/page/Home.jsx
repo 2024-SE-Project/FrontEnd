5e75e8d945de107635dded5d57378876d7acbad7
SJ Mac Pro <sjcaptain@SJui-MacBookPro.local>
2024-05-09 15:40:12 +0900

    HomePage image updated & Login Page modified

--- hgu_tmaew_front/src/page/Home.jsx
+++ hgu_tmaew_front/src/page/Home.jsx
@@ -2,19 +2,19 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import { Parallax } from 'react-parallax';
 import './css/Home.css'; // 홈 페이지 스타일을 위한 CSS 파일
-import logo from '../assets/logo_white.svg'; // 이미지의 상대 경로
-import kuyper from '../assets/dorm/kuyper.jpg';
-import torrey from '../assets/dorm/torrey.jpg';
-import carmichael from '../assets/dorm/carmichael.jpg';
-import bethel from '../assets/dorm/bethel.jpg';
-import yeolsong from '../assets/dorm/yeolsong.jpg';
-import grace from '../assets/dorm/grace.jpg';
+import Logo from '../assets/logo_white.svg'; // 이미지의 상대 경로
+import Kuyper from '../assets/dorm/kuyper.jpg';
+import Torrey from '../assets/dorm/torrey.jpg';
+import Carmichael from '../assets/dorm/carmichael.jpg';
+import Bethel from '../assets/dorm/bethel.jpg';
+import Yeolsong from '../assets/dorm/yeolsong.jpg';
+import Grace from '../assets/dorm/grace.jpg';
 
 const Home = () => {
 
   function ParallaxComponent({ url, title }) {
     return (
-      <Parallax bgImage={url} strength={1000} bgImageStyle={{ backgroundSize: 'contain', backgroundRepeat: 'no-repeat', backgroundPosition: 'center' }}>
+      <Parallax bgImage={url} strength={600} blur={{min: -5, max: 15}} >
         <div className="parallax-container">
           <div className="parallax-title">{title}</div>
         </div>
@@ -25,7 +25,7 @@ const Home = () => {
   return (
     <div className="home">
       <header className="home-header">
-        <img src={logo} className="home-logo" alt="logo" />
+        <img src={Logo} className="home-logo" alt="logo" />
       </header>
       <main className="home-content">
         <p className="home-content-title">Handong Team Meeting Archiving<br />And Exchange Web Platform</p>
@@ -33,6 +33,7 @@ const Home = () => {
         <Link to="/login" className="loginButton">Start</Link>
       </main>
 
+      {/* 이 부분 br/을 화면 비율에 맞게 적용될 수 있도록 수정할 것! */}
       <br />
       <br />
       <br />
@@ -52,12 +53,12 @@ const Home = () => {
       <br />
 
       <div className="home-dormInfo">
-        <ParallaxComponent url={kuyper} title="Kuyper" />
-        <ParallaxComponent url={torrey} title="Torrey" />
-        <ParallaxComponent url={carmichael} title="Carmichael" />
-        <ParallaxComponent url={bethel} title="Bethel" />
-        <ParallaxComponent url={yeolsong} title="Yeolsong" />
-        <ParallaxComponent url={grace} title="Grace" />
+        <ParallaxComponent url={Kuyper} title="Kuyper" />
+        <ParallaxComponent url={Torrey} title="Torrey" />
+        <ParallaxComponent url={Carmichael} title="Carmichael" />
+        <ParallaxComponent url={Bethel} title="Bethel" />
+        <ParallaxComponent url={Yeolsong} title="Yeolsong" />
+        <ParallaxComponent url={Grace} title="Grace" />
       </div>
     </div>
   );
--- hgu_tmaew_front/src/page/Login.jsx
+++ hgu_tmaew_front/src/page/Login.jsx
@@ -1,31 +1,82 @@
-// LoginPage.jsx
+// // LoginPage.jsx
+// import React from 'react';
+// import { useNavigate } from 'react-router-dom';
+// import { GoogleLogin, GoogleOAuthProvider } from '@react-oauth/google';
+
+// function LoginPage() {
+//     const navigate = useNavigate();
+
+//     const responseGoogle = (response) => {
+//         console.log(response);
+//         // 여기서 response 객체를 사용하여 사용자 정보를 얻을 수 있음
+//         // 예를 들어, response 객체의 구조에 따라 사용자의 구글 프로필 정보를 얻는 방식이 달라질 수 있음!!!
+
+//         // 로그인 성공 후 홈 페이지로 리다이렉트
+//         navigate('/');
+//     }
+
+//     // 구글 클라이언트 ID를 여기에 입력
+//     const clientId = "your-google-client-id-here";
+
+//     return (
+//         <GoogleOAuthProvider clientId={clientId}> {/* 추가된 GoogleOAuthProvider */}
+//             <div>
+//                 <h2>로그인 페이지</h2>
+//                 <GoogleLogin
+//                     onSuccess={responseGoogle}
+//                     onError={responseGoogle}
+//                 />
+//             </div>
+//         </GoogleOAuthProvider>
+//     );
+// }
+
+// export default LoginPage;
+
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
 import { GoogleLogin, GoogleOAuthProvider } from '@react-oauth/google';
+import './css/Login.css';
 
 function LoginPage() {
     const navigate = useNavigate();
 
     const responseGoogle = (response) => {
         console.log(response);
-        // 여기서 response 객체를 사용하여 사용자 정보를 얻을 수 있음
-        // 예를 들어, response 객체의 구조에 따라 사용자의 구글 프로필 정보를 얻는 방식이 달라질 수 있음!!!
-        
-        // 로그인 성공 후 홈 페이지로 리다이렉트
-        navigate('/');
+        navigate('/');  // 로그인 성공 후 홈 페이지로 리다이렉트
     }
 
-    // 구글 클라이언트 ID를 여기에 입력
-    const clientId = "your-google-client-id-here";
+    const clientId = "your-google-client-id-here";  // 실제 구글 클라이언트 ID로 대체
 
     return (
-        <GoogleOAuthProvider clientId={clientId}> {/* 추가된 GoogleOAuthProvider */}
-            <div>
-                <h2>로그인 페이지</h2>
-                <GoogleLogin
-                    onSuccess={responseGoogle}
-                    onError={responseGoogle}
-                />
+        <GoogleOAuthProvider clientId={clientId}>
+            <div className="signin-container">
+                <div className="left-panel">
+                    <h1 className="Sign-in-title">Sign in to</h1>
+                    <h2 className="Sign-in-subTitle">RAONz is simply</h2>
+                    <p className="Sign-in-dc">This platform will support the student<br /> support team and team executives in<br /> fostering a stronger team culture and<br /> developing leadership skills<br /> university-wide.</p>
+                </div>
+                <div className="right-panel">
+                    <div className="signin-box">
+                        <h1>Welcome to RAONz</h1>
+                        <h2>Sign in</h2>
+                        <GoogleLogin
+                            onSuccess={responseGoogle}
+                            onError={responseGoogle}
+                            render={renderProps => (
+                                <button onClick={renderProps.onClick} disabled={renderProps.disabled} className="google-signin-btn">
+                                    Sign in with Google
+                                </button>
+                            )}
+                        />
+                        <button className="link-button" /* onClick={handleForgotPassword} */ >Forgot Password</button>
+                        <div className="signup-link">
+                            No Account? <button className="link-button" /* onClick={handleSignUp } */ >Sign up</button>
+                        </div>
+
+
+                    </div>
+                </div>
             </div>
         </GoogleOAuthProvider>
     );
