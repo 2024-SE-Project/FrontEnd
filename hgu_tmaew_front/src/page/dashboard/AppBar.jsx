621866705fb9f3576a868f2dadb8732aec232125
SJ Mac Pro <sjcaptain@SJui-MacBookPro.local>
2024-06-05 17:11:49 +0900

    AppBar contents updated

--- hgu_tmaew_front/src/page/dashboard/AppBar.jsx
+++ hgu_tmaew_front/src/page/dashboard/AppBar.jsx
@@ -1,15 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { AppBar, Toolbar, Typography, IconButton, Avatar, Box } from '@mui/material';
 import MenuIcon from '@mui/icons-material/Menu';
 import AddIcon from '@mui/icons-material/Add';
 import MessageIcon from '@mui/icons-material/Message';
 import NotificationsIcon from '@mui/icons-material/Notifications';
 import ArrowDropDownIcon from '@mui/icons-material/ArrowDropDown';
+import DialogTag from '../dialog/DialogTag';
 import '../css/Appbar.css';
 
 export default function Appbar({ toggleDrawer }) {
     const name = localStorage.getItem("name");
-    
+    const [openCreate, setOpenCreate] = useState(false);
+    const [posts, setPosts] = useState([]);
+
+    const handleClickOpenCreate = () => {
+        setOpenCreate(true);
+    };
+
+    const handleCloseCreate = (newPost) => {
+        if (newPost) {
+            setPosts([newPost, ...posts]);
+        }
+        setOpenCreate(false);
+    };
+
     return (
         <AppBar position="fixed" className="appbar">
             <Toolbar>
@@ -26,9 +40,16 @@ export default function Appbar({ toggleDrawer }) {
                     RAONz
                 </Typography>
                 <Box className="icon-group">
-                    <IconButton color="inherit" className="icon-button">
+                    <IconButton color="inherit" className="icon-button" onClick={handleClickOpenCreate}>
                         <AddIcon />
                     </IconButton>
+                    {openCreate && (
+                        <DialogTag
+                            open={openCreate}
+                            title={'추가하기'}
+                            onClose={handleCloseCreate}
+                        />
+                    )}
                     <IconButton color="inherit" className="icon-button">
                         <MessageIcon />
                     </IconButton>
