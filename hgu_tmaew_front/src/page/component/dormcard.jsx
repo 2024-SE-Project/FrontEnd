3ed9f9776df64a6ef77f6f4b19ac00f4d70b415f
LeeMineo <lms4882@gmail.com>
2024-06-07 16:58:45 +0900

    change home UI
    각 RC 별 설명 추가함요

--- hgu_tmaew_front/src/page/component/dormcard.jsx
+++ hgu_tmaew_front/src/page/component/dormcard.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import '../css/Dormcard.css';
 
-const DormCard = ({ name, className, CharacterSvg, IconSvg }) => {
+const DormCard = ({ name, className, CharacterSvg, IconSvg, description }) => {
   return (
     <div className={`dorm-card ${className}`}>
       <div className="big-image">
@@ -10,7 +10,10 @@ const DormCard = ({ name, className, CharacterSvg, IconSvg }) => {
       <div className="small-image">
         <IconSvg className="icon-image" />
       </div>
-      <div className="dorm-name">{name}</div>
+      <div className="dorm-word">
+        <div className="dorm-name">{name}</div>
+        <div className="dorm-description">{description}</div>
+      </div>
     </div>
   );
 };
