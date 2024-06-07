b866376c3fda2ec711ae113716484d304965c94d
choijh4161@gmail.com <21900764@handong.ac.kr>
2024-06-07 20:37:37 +0900

    feat: multi team 구현

--- hgu_tmaew_front/src/page/MyTeam.jsx
+++ hgu_tmaew_front/src/page/MyTeam.jsx
@@ -1,19 +1,27 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
-import { useNavigate } from 'react-router-dom';
+import { useNavigate, NavLink } from 'react-router-dom';
 import '../page/css/MyTeam.css';
 import CreateTeamModal from './component/CreateTeamModal';
 import { Button, Dialog, DialogActions, DialogContent, DialogContentText, DialogTitle } from '@mui/material';
 
 export default function MyTeam() {
-  const [teamInfo, setTeamInfo] = useState(null);
+  const [teams, setTeams] = useState([]);
   const [loading, setLoading] = useState(true);
   const [showModal, setShowModal] = useState(false);
   const [openDeleteDialog, setOpenDeleteDialog] = useState(false);
+  const [isScrolled, setIsScrolled] = useState(false);
+  const [selectedTeam, setSelectedTeam] = useState(null);
   const navigate = useNavigate();
 
   useEffect(() => {
-    const fetchTeamInfo = async () => {
+    const handleScroll = () => {
+      setIsScrolled(window.scrollY > 50);
+    };
+
+    window.addEventListener('scroll', handleScroll);
+
+    const fetchTeams = async () => {
       const token = localStorage.getItem('token');
       if (!token) {
         console.error('Token not found');
@@ -27,21 +35,20 @@ export default function MyTeam() {
             Authorization: `Bearer ${token}`,
           },
         });
-        const data = response.data;
-        if (data) {
-          setTeamInfo(data[0]); // Assuming the user is part of at least one team and we're taking the first one
-        } else {
-          setTeamInfo(null);
-        }
+        setTeams(response.data || []);
       } catch (error) {
         console.error('Error fetching data:', error);
-        setTeamInfo(null);
+        setTeams([]);
       } finally {
         setLoading(false);
       }
     };
 
-    fetchTeamInfo();
+    fetchTeams();
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
   }, [navigate]);
 
   const handleCreateTeamClick = () => {
@@ -52,89 +59,93 @@ export default function MyTeam() {
     setShowModal(false);
   };
 
-  const handleEditClick = () => {
+  const handleEditClick = (team) => {
     // Edit team logic goes here
   };
 
-  const handleExit = () => {
+  const handleExit = (team) => {
+    setSelectedTeam(team);
     setOpenDeleteDialog(true);
   };
 
   const handleDelete = async () => {
     const token = localStorage.getItem('token');
     try {
-      await axios.delete(`https://likelion.info:443/team/delete/${teamInfo.id}`, {
+      await axios.delete(`https://likelion.info:443/team/delete/${selectedTeam.id}`, {
         headers: {
           Authorization: `Bearer ${token}`
         }
       });
+      setTeams((prevTeams) => prevTeams.filter(team => team.id !== selectedTeam.id));
       setOpenDeleteDialog(false);
-      navigate('/dashboard', { replace: true });
+      setSelectedTeam(null);
     } catch (error) {
       console.error('Error exiting team:', error);
     }
-  };  
+  };
 
   if (loading) {
     return <div>Loading...</div>;
   }
-
-  if (!teamInfo) {
-    return (
-      <div className="my-team">
-        <div className="team-header">
-          <h2>나의 팀</h2>
-        </div>
-        <div className="team-content no-team">
-          <p>현재 팀에 소속되어있지 않습니다! 초대를 기다리거나, 생성하세요.</p>
-          <button className="create-team-button" onClick={handleCreateTeamClick}>팀 생성하기</button>
-        </div>
-        {showModal && <CreateTeamModal onClose={handleModalClose} />}
-      </div>
-    );
-  }
-
-  const imageUrl = teamInfo.imgURL || "https://storage.googleapis.com/raonz_post_image/cat9.jpg";
 
   return (
     <div className="my-team">
       <div className="team-header">
         <h2>나의 팀</h2>
       </div>
-      <div className="team-content">
-        <div className="team-info">
-          <div className="team-photo">
-            <h4>대표 사진</h4>
-            <img src={imageUrl} alt="대표사진" />
-          </div>
-          <div className="team-message">
-            <h4>대표 인사말</h4>
-            <p>{teamInfo.content || "팀 소개말이 없습니다."}</p>
-            <span className="no-edits">수정권한 없음</span>
-          </div>
+      {teams.length === 0 ? (
+        <div className="team-content no-team">
+          <p>현재 팀에 소속되어있지 않습니다! 초대를 기다리거나, 생성하세요.</p>
+          <button className="create-team-button" onClick={handleCreateTeamClick}>팀 생성하기</button>
+          {showModal && <CreateTeamModal onClose={handleModalClose} />}
         </div>
-        <div className="team-details">
-          <h3>{teamInfo.name}</h3>
-          <div className="team-out">
-            <span className="leave-team" onClick={handleExit}>탈퇴하기</span>
-            <Dialog
-              open={openDeleteDialog}
-              onClose={() => setOpenDeleteDialog(false)}
-            >
-              <DialogTitle>팀 탈퇴</DialogTitle>
-              <DialogContent>
-                <DialogContentText>정말로 팀을 나가시겠습니까?</DialogContentText>
-              </DialogContent>
-              <DialogActions>
-                <Button onClick={() => setOpenDeleteDialog(false)} color="primary">취소</Button>
-                <Button onClick={handleDelete} color="secondary">나가기</Button>
-              </DialogActions>
-            </Dialog>
-            <span className="edit-team" onClick={handleEditClick}>수정</span>
+      ) : (
+        teams.map((team) => (
+          <div key={team.id} className="team-content">
+            <div className="team-info">
+              <div className="team-photo">
+                <h4>대표 사진</h4>
+                <img src={team.imgURL || "https://storage.cloud.google.com/raonz_post_image/cat9.jpg"} alt="대표사진" />
+              </div>
+              <div className="team-message">
+                <h4>대표 인사말</h4>
+                <p>{team.content || "팀 소개말이 없습니다."}</p>
+                <span className="no-edits">수정권한 없음</span>
+              </div>
+            </div>
+            <div className="team-details">
+              <h3>{team.name}</h3>
+              
+              <div className="team-out">
+                  <span className="leave-team" onClick={() => handleExit(team)}>탈퇴하기</span>
+                  <span className="edit-team" onClick={() => handleEditClick(team)}>수정</span>
+                </div>
+            </div>
           </div>
-        </div>
-      </div>
+        ))
+      )}
       {showModal && <CreateTeamModal onClose={handleModalClose} />}
+      <NavLink
+        onClick={handleCreateTeamClick}
+        className={`ref-floating-button ${isScrolled ? 'h_event2' : ''}`}
+      >
+        <div>
+          <span className="ref-menu-icon">+</span>
+        </div>
+      </NavLink>
+      <Dialog
+        open={openDeleteDialog}
+        onClose={() => setOpenDeleteDialog(false)}
+      >
+        <DialogTitle>팀 탈퇴</DialogTitle>
+        <DialogContent>
+          <DialogContentText>정말로 팀을 나가시겠습니까?</DialogContentText>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={() => setOpenDeleteDialog(false)} color="primary">취소</Button>
+          <Button onClick={handleDelete} color="secondary">나가기</Button>
+        </DialogActions>
+      </Dialog>
     </div>
   );
 }
