6bc56945935cff7175e2c7f0627dd029a889e114
LeeMineo <lms4882@gmail.com>
2024-06-07 18:55:14 +0900

    명예의전당 수정
    더예쁘게

--- hgu_tmaew_front/src/page/Ranking.jsx
+++ hgu_tmaew_front/src/page/Ranking.jsx
@@ -3,33 +3,10 @@ import '../page/css/Ranking.css';
 import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
 
-const samplePosts = [
-  {
-    id: 1,
-    name: '최희열 교수님 팀',
-    image: 'https://storage.googleapis.com/raonz_post_image/cat.jpg',
-    score: 500,
-  },
-  {
-    id: 2,
-    name: '남재창 교수님 팀',
-    image: 'https://storage.googleapis.com/raonz_post_image/cat1.jpg',
-    score: 300,
-  },
-  {
-    id: 3,
-    name: '이희진 교수님 팀',
-    image: 'https://storage.googleapis.com/raonz_post_image/cat2.jpg',
-    score: 100,
-  },
-  // ...other sample data
-];
-
 export default function Ranking() {
   const [rankingData, setRankingData] = useState([]);
   const [year, setYear] = useState(2023);
   const [loading, setLoading] = useState(true);
-  const [showModal, setShowModal] = useState(false);
   const navigate = useNavigate();
 
   const handleChangeYear = () => {
@@ -69,14 +46,6 @@ export default function Ranking() {
     fetchRankingInfo();
   }, [navigate]);
 
-  const handleCreatepostClick = () => {
-    setShowModal(true);
-  };
-
-  const handleModalClose = () => {
-    setShowModal(false);
-  };
-
   if (loading) {
     return <div>Loading...</div>;
   }
@@ -85,10 +54,12 @@ export default function Ranking() {
     <div className="ranking-container">
       <header className="ranking-header">
         <h1>명예의 전당</h1>
-        <button className="year-button" onClick={handleChangeYear}>
-          년도 바꾸기
-        </button>
-        <span className="year-label">기준: {year}년</span>
+        <div className="year-container">
+          <button className="year-button" onClick={handleChangeYear}>
+            연도 바꾸기
+          </button>
+          <span className="year-label">기준: {year}년</span>
+        </div>
       </header>
       <main className="ranking-main">
         <div className="top-rankings">
@@ -101,19 +72,19 @@ export default function Ranking() {
             </div>
           ))}
         </div>
-        <div className="other-rankings">
+      </main>
+      <div className="other-rankings">
           <h2>나머지 등수</h2>
           <div className="ranking-list">
             {rankingData.slice(3).map((post, index) => (
               <div key={post.id} className="ranking-item">
-                <div className="ranking-name">{post.title}</div>
                 <div className="ranking-position">{index + 4}등</div>
+                <div className="ranking-name">{post.title}</div>
                 <div className="ranking-score">❤️ {post.likeCount}</div>
               </div>
             ))}
           </div>
         </div>
-      </main>
     </div>
   );
 }
