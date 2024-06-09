54d1adbb1ffcb262641964150f7eb1b408d94163
SJ Mac Pro <sjcaptain@SJui-MacBookPro.local>
2024-06-09 22:32:23 +0900

    Main Page comment reload func update

--- hgu_tmaew_front/src/page/Main.jsx
+++ hgu_tmaew_front/src/page/Main.jsx
@@ -26,10 +26,7 @@ import {
   DialogActions,
   DialogContent,
   DialogContentText,
-  DialogTitle,
-  Select,
-  FormControl,
-  InputLabel
+  DialogTitle
 } from '@mui/material';
 import FavoriteIcon from '@mui/icons-material/Favorite';
 import MoreVertIcon from '@mui/icons-material/MoreVert';
@@ -62,7 +59,6 @@ export default function Main() {
   const [isScrolled, setIsScrolled] = useState(false);
   const navigate = useNavigate();
   const [userInfo, setUserInfo] = useState(null);
-  const [selectedTeam, setSelectedTeam] = useState('');
 
   const handleClick = (event, post) => {
     setAnchorEl(event.currentTarget);
@@ -114,6 +110,7 @@ export default function Main() {
   
         const commentId = await response.json();
         console.log(`Comment added with ID: ${commentId}`);
+        window.location.reload();
         
         // Clear the comment for the specific post
         setComments(prevComments => ({ ...prevComments, [postId]: '' }));
@@ -303,24 +300,8 @@ export default function Main() {
       </Helmet>
 
       <Box className="content-container">
-        <FormControl variant="outlined" style={{ minWidth: 120, marginBottom: 16 }}>
-          <InputLabel>Team</InputLabel>
-          <Select
-            value={selectedTeam}
-            onChange={(e) => setSelectedTeam(e.target.value)}
-            label="Team"
-          >
-            <MenuItem value="">
-              <em>All Teams</em>
-            </MenuItem>
-            {sampleTeams.map((team) => (
-              <MenuItem key={team.id} value={team.name}>{team.name}</MenuItem>
-            ))}
-          </Select>
-        </FormControl>
-
         <Box className="posts-container">
-          {Array.isArray(posts) && posts.filter(post => selectedTeam === '' || post.team === selectedTeam).map((post, index) => (
+          {Array.isArray(posts) && posts.map((post, index) => (
             <Card key={post.postId} className="post-card">
               <CardContent>
                 <Box display="flex" alignItems="center" mb={2}>
